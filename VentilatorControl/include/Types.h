304c7f54d69d07be8ca9346840e1d46523878d26
smaciel <sheldonmaciel@gmail.com>
2020-04-23 11:51:13 -0600

    Moved typedefs in types.h file

---
+++ VentilatorControl/include/Types.h
@@ -0,0 +1,12 @@
+#ifndef __TYPES_H__
+#define __TYPES_H__
+
+typedef enum{
+    INHALE_RAMP,
+    INHALE_HOLD,
+    EXHALE_RAMP,
+    EXHALE_HOLD,
+    IDLE
+}BreathCycleStep;
+
+#endif  // __TYPES_H__
