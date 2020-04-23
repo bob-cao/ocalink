adfb7ff5f1f255be7e1274177033ec0261ebef37
smaciel <sheldonmaciel@gmail.com>
2020-04-23 15:14:36 -0600

    Moved linear calculation function into helpers

--- VentilatorControl/src/main.cpp
+++ VentilatorControl/src/main.cpp
@@ -94,23 +94,6 @@ void cycleStateHandler (void)
   }
 }
 
-// Compensation for DC offset in pressure achieved.
-// Found by linear regression of the following experimental datapoints:
-// Target Val	Setpoint Req'd
-// 15       	16
-// 20	        21
-// 25	        27.9
-// 30	        33.5
-// 35	        38.5
-// 40	        44.5
-// 45	        49.25
-// 50	        55
-
-double linearRemapSetpointCompensation (double setpoint)
-{
-  return (1.116785714*setpoint)-0.5892857143;
-}
-
 void cycleStateSetpointHandler(void)
 {
   // TODO: HIGH breakout into separate file and rewrite for readability
