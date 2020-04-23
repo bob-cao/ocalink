3a4b6c98c6be1400b98cc3397dda993df93c5610
smaciel <sheldonmaciel@gmail.com>
2020-04-23 16:11:57 -0600

    Put a TODO comment with old and new names to update when there is enough time to keep namking consistent with C++ naming convention

--- VentilatorControl/src/variables.h
+++ VentilatorControl/src/variables.h
@@ -1,6 +1,61 @@
 #ifndef __VARIABLES_H__
 #define __VARIABLES_H__
 
+
+
+// TODO: (LOW Priority) Update variable names to be consistent with C++ standard
+
+// OLD NAME                                        -> NEW NAME
+
+// patientCircuitPressure				            -> PatientCircuitPressure
+// venturiDifferentialPressure			            -> VenturiDifferentialPressure
+
+// pressure_reading			        	        -> pressureReading
+// blower_speed				        	        -> blowerSpeed
+// string_from_pi				       	            -> stringFromPi
+// property_name				    	            -> propertyName
+// argument_value			    		            -> argumentValue
+// peep_low_alarm			    		            -> peepLowAlarm
+// peep_alarm				        	            -> peepAlarm
+// pip_alarm				        	            -> pipAlarm
+// PeepPressureCentimetersH2O			            -> peepPressureCentimetersH2O
+// PipPressureCentimetersH2O			            -> pipPressureCentimetersH2O
+// RespritoryRate				    	            -> respritoryRate
+// InhalationExhalationRatio			            -> inhalationExhalationRatio
+// FlowOfOxygen					                -> flowOfOxygen
+// IEScalingFactor				       	            -> ScalingFactorIE
+// PeepAlarmTimer				    	            -> peepAlarmTimer
+// PipAlarmTimer				    	            -> pipAlarmTimer
+// DisconnectAlarmTimer			    	        -> disconnectAlarmTimer
+// ApneaTimer				        	            -> apneaTimer
+// buzzer_state					                -> buzzerState
+// low_red					        	            -> lowRed
+// low_amber				        	            -> lowAmber
+// low_green					                    -> lowGreen
+// low_perywinkle				    	            -> lowPeriwinkle
+// alarm_state					                    -> alarmState
+// CurrTimeInCycleMilliseconds			            -> currTimeInCycleMilliseconds
+// CycleStartTimeFromSysClockMilliseconds		    -> cycleStartTimeFromSysClockMilliseconds
+// ControlLoopStartTimeMilliseconds		        -> controlLoopStartTimeMilliseconds
+// ControlLoopInitialStabilizationTimeMilliseconds -> controlLoopInitialStabilizationTimeMilliseconds
+// InhaleRampDurationMilliseconds			        -> inhaleRampDurationMilliseconds
+// inhaleDurationMilliseconds                      -> inhaleDurationMilliseconds
+// ExhaleDurationMilliseconds			            -> exhaleDurationMilliseconds
+// BreathCycleDurationMilliseconds			        -> breathCycleDurationMilliseconds
+// PressureSensorLastStatusRead			        -> pressureSensorLastStatusRead
+// TimeOfLastSolenoidToggleMilliseconds		    -> timeOfLastSolenoidToggleMilliseconds
+// pressure_system_input				            -> pressureSystemInput
+// blower_output_speed_in_percentage		        -> blowerOutputSpeedInPercentage
+// pinch_valve_output_openness_in_percentage	    -> pinchValveOutputOpennessInPercentage
+// CurrPressureSetpointCentimetersH2O		        -> currPressureSetpointCentimetersH2O
+// Blower_Kp					                    -> blowerKp
+// Blower_Ki					                    -> blowerKi
+// Blower_Kd					                    -> blowerKd
+// Blower_PID					                    -> blowerPID
+// PinchValve_PID                                  -> pinchValvePID
+
+
+
 // ----------------------------------LIBRARIES--------------------------------------- //
 #include "includes.h"
 // ----------------------------------LIBRARIES--------------------------------------- //
